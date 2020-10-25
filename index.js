f5e3ca3916a492e2b46644d507f9654ff7311ee5
Mateus Rodrigues <57692147+Halste4d@users.noreply.github.com>
2020-10-25 01:34:34 -0300

    fastcord

---
+++ index.js
@@ -0,0 +1,39 @@
+const prompt = require('prompt-sync')();
+const files = require('./utils/sourceCode.json');
+const fs = require('fs');
+const chalk = require('chalk')
+
+const Bot = () => {
+    const botName = prompt(chalk.magenta('[Name] Bot name: '));
+    let botPrefix = prompt(chalk.magenta('[Prefix] Bot prefix: '));
+    const botToken = prompt(chalk.magenta('[Token] Bot token: '));
+    files['.env'].code = `TOKEN=${botToken}\nPREFIX=${botPrefix}`;
+
+    if(!botPrefix) botPrefix = '!'
+    if(!botName) return console.log(chalk.red('[ERR] Please, inform your bot name.'));
+    if(!botToken) return console.log(chalk.red('[ERR] Please, inform TOKEN correctly.'));
+
+    createSourceBot(botName)
+}
+
+function createSourceBot(botName) {
+    const root = `${botName}-bot`
+
+    fs.mkdir(`${root}`, { recursive: true }, (err) => {
+        if (err) throw err;
+    });
+
+    for(file in files) {
+        fs.mkdir(`${root}${files[file].path}`, { recursive: true }, (err) => { 
+            if (err) throw err;
+        });
+
+        fs.writeFile(`${root}${files[file].path}${file}`, `${files[file].code}`, (err) => {
+            if (err) throw err;
+        });
+    }
+    
+    console.log(chalk.green(`The bot ${botName} was created.`))
+}
+
+Bot()
