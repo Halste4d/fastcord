4ba96924da9165d99e136f648bd11635c0090640
Arthur Costa <fairygloxinia@protonmail.com>
2020-10-29 18:16:07 -0300

    Optimizing code and correcting semantics

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const prompt = require('prompt-sync')();
 const files = require('./utils/sourceCode.json');
 const fs = require('fs');
-const chalk = require('chalk')
+const chalk = require('chalk');
 
 const Bot = () => {
     const botName = prompt(chalk.magenta('[Name] Bot name: '));
@@ -9,31 +9,31 @@ const Bot = () => {
     const botToken = prompt(chalk.magenta('[Token] Bot token: '));
     files['.env'].code = `TOKEN=${botToken}\nPREFIX=${botPrefix}`;
 
-    if(!botPrefix) botPrefix = '!'
+    if(!botPrefix) botPrefix = '!';
     if(!botName) return console.log(chalk.red('[ERR] Please, inform your bot name.'));
     if(!botToken) return console.log(chalk.red('[ERR] Please, inform TOKEN correctly.'));
 
-    createSourceBot(botName)
+    createSourceBot(botName);
 }
 
 function createSourceBot(botName) {
-    const root = `${botName}-bot`
+    const root = botName + '-';
 
-    fs.mkdir(`${root}`, { recursive: true }, (err) => {
+    fs.mkdir(root, { recursive: true }, (err) => {
         if (err) throw err;
     });
 
-    for(file in files) {
-        fs.mkdir(`${root}${files[file].path}`, { recursive: true }, (err) => { 
+    for (var file = 0; file < files.length; file++) {
+        fs.mkdir(root + files[file].path, { recursive: true }, (err) => { 
             if (err) throw err;
         });
 
-        fs.writeFile(`${root}${files[file].path}${file}`, `${files[file].code}`, (err) => {
+        fs.writeFile(root + files[file].path + file, files[file].code, (err) => {
             if (err) throw err;
         });
     }
     
-    console.log(chalk.green(`The bot ${botName} was created.`))
+    console.log(chalk.green(`The bot ${botName} was created.`));
 }
 
-Bot()
+Bot();
