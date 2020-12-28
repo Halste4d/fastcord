688121e4e116329f196174ed206b328c5ee696d7
remfan <remfan@outlook.com.br>
2020-12-28 03:02:35 -0300

    Adding option to create bot with eris and fixing download dependencies

--- index.js
+++ index.js
@@ -1,39 +1,46 @@
 const prompt = require('prompt-sync')();
 const files = require('./utils/sourceCode.json');
 const fs = require('fs');
-const chalk = require('chalk')
+const chalk = require('chalk');
+const child_process = require('child_process');
 
 const Bot = () => {
-    const botName = prompt(chalk.magenta('[Name] Bot name: '));
-    let botPrefix = prompt(chalk.magenta('[Prefix] Bot prefix: '));
+    const botName = prompt(chalk.magenta('[Name] Bot name: ')) || 'Untitled';
+    const botPrefix = prompt(chalk.magenta('[Prefix] Bot prefix: ')) || '!';
     const botToken = prompt(chalk.magenta('[Token] Bot token: '));
+
+    if (!botToken) throw new Error('Invalid bot token!');
+
+    const botLibrary = prompt(chalk.magenta('[Library] Bot library[discord.js, eris]: ')) || 'discord.js';
+
+    if (!['eris', 'discord.js'].includes(botLibrary)) throw new Error('Invalid bot library!');
+
     files['.env'].code = `TOKEN=${botToken}\nPREFIX=${botPrefix}`;
 
-    if(!botPrefix) botPrefix = '!'
-    if(!botName) return console.log(chalk.red('[ERR] Please, inform your bot name.'));
-    if(!botToken) return console.log(chalk.red('[ERR] Please, inform TOKEN correctly.'));
-
-    createSourceBot(botName)
+    createSourceBot(botName, botLibrary)
 }
 
-function createSourceBot(botName) {
-    const root = `${botName}-bot`
+function createSourceBot(botName, botLibrary) {
+    const root = botName + '-bot'
 
-    fs.mkdir(`${root}`, { recursive: true }, (err) => {
+    fs.mkdir(root, { recursive: true }, (err) => {
         if (err) throw err;
     });
 
-    for(file in files) {
-        fs.mkdir(`${root}${files[file].path}`, { recursive: true }, (err) => { 
-            if (err) throw err;
-        });
+    fs.writeFile(root + files['.env'].path + '.env', files['.env'].code, { recursive: true }, (err) => { 
+        if (err) throw err;
+    });
 
-        fs.writeFile(`${root}${files[file].path}${file}`, `${files[file].code}`, (err) => {
-            if (err) throw err;
+    fs.writeFile(root + files['index.js'].path + 'index.js', files['index.js'].code[botLibrary], (err) => {
+        if (err) throw err;
+        else child_process.exec('cd ' + root + ' && npm init -y && npm install dotenv ' + botLibrary, function (err, out) {
+            if (err) throw 'Error when trying to instal dependencies: ' + err.message;
+ 
+            console.log(chalk.yellow('Dependencies installed...'));
         });
-    }
-    
-    console.log(chalk.green(`The bot ${botName} was created.`))
+    });
+
+    console.log(chalk.green('The bot ' + botName + ' was created.'));
 }
 
-Bot()
+Bot();
